dd6e9a362374c9cc3121c88c3755dbd0438ba3fb
ByeongGil Jung <jbkcose@gmail.com>
2018-02-14 15:05:33 +0900

    [18-02-14] - Connecting 'Heroku' and adding 'PAPAGO Token' and 'Slack Token' - Adding the Heroku settings on Python

--- data.py
+++ data.py
@@ -1,12 +1,14 @@
 from enum import Enum
+import os
 
+# For using Heroku
 class SlackUrls(Enum):
-    base_url = ''
-    token = ''
+    base_url = 'https://slack.com/api/rtm.connect?token='
+    token = os.environ.get('SLACK_BOT_TOKEN')
 
 class NaverUrls(Enum):
-    client_id = ''
-    client_secret = ''
+    client_id = os.environ.get('PAPAGO_CLIENT_ID')
+    client_secret = os.environ.get('PAPAGO_CLIENT_SECRET')
     """
     < 네이버 파파고 API >
     1. 네이버 개발자 센터 파파고 API 신청 (https://developers.naver.com/apps/#/register?api=ppg_n2mt).
